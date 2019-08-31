f684cde68327705798c9f8b8f9b5af355610b557
bruno1576 <brunosalve1@hotmail.com>
2019-08-31 19:41:16 -0300

    Box3

---
+++ box3/sort.js
@@ -0,0 +1,23 @@
+function superSort(array)
+{
+    let array_sorteado = [];
+    let temporario = 0;
+    for (i = 0; i < array.length; i++) { 
+    
+        for (j = 0; j < array.length; j++) { 
+    
+            if (array[i] < array[j]){
+
+                temporario = array[i];
+                array[i] =  array[j];
+                array[j] = temporario;
+
+             }
+        }
+
+
+      }
+      return array;
+}
+
+module.exports = superSort;
